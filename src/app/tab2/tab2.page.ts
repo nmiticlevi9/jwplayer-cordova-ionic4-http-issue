a6e0372de1ff841958aaa436acbbb302108f9591
Nemanja Mitic <n.mitic@levi9.com>
2019-12-25 14:13:54 +0100

    Added jwplayer to empty ionic cordova project

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit() {
+    // @ts-ignore
+    jwplayer('jwplayerid').setup({
+      // autostart: this.autoPlay,
+      autostart: false,
+      controls: true,
+      file: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+      width: 400,
+      stretching: 'uniform',
+      aspectratio: '16:9',
+      sdkplatform: 'true',
+    });
+  }
 
 }
